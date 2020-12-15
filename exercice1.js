f8163a6ca6f1f6f18325f0e91ed47c2de024477c
Sylvie-B <unsolo@hotmail.fr>
2020-12-15 20:24:10 +0100

    boucler sur le tableau créer nouveau li

--- exercice1.js
+++ exercice1.js
@@ -12,11 +12,25 @@ let articles = [
 
 // Votre code à partir d'ici :
 
-let list = document.getElementById("listeCourse");
+let list = document.getElementById("listeCourse");      // get list
 
-let newItem = document.createElement('li');
 
-newItem.innerHTML = articles[0];
-console.log(newItem);
-list.appendChild(newItem);
+// for each item of list
+for (let i = 0 ; i < articles.length ; i++){
+    //  create element li
+    let item = document.createElement('li');
+    item.innerHTML = articles[i];
+    list.appendChild(item);
+}
+
+// create champ & button
+document.getElementById('newItem').focus();
+// add item on
+document.getElementById('add').addEventListener('click', function (){
+    let newItem = document.createElement('li');
+    newItem.innerHTML = document.getElementById('newItem').value;
+    list.appendChild(newItem);
+    document.getElementById('newItem').value = "";
+    document.getElementById('newItem').focus();
+})
 
